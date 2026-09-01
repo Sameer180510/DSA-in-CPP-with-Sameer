1a494ced4c1cb150c07f06c2cd885e0caf0da051
Sameer Rahangdale <166120401+Sameer180510@users.noreply.github.com>
2026-09-02 01:10:43 +0530

    Added Static vs Dynamic Memory Management

---
+++ 49 - Memory Management/Memory.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Variable ke liye heap memory allocate karo
+    // Agar int ke liye chahiye to...
+    int *ptr = new int;
+    cout << "Address of ptr: " << ptr << endl;
+
+    // Inserting value in ptr
+    *ptr = 18; // Value assign
+    cout << "Value at ptr: " << *ptr << endl;
+
+    // Memory allocation for float value
+    float *ptr2 = new float;
+    *ptr2 = 7.45;
+    cout << "Address of ptr2: " << ptr2 << endl;
+    cout << "Value at ptr2: " << *ptr2 << endl;
+
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int *p = new int[n];
+
+    // Insert value in array
+    for (int i = 0; i < n; i++)
+    {
+        p[i] = i + 1;
+    }
+
+    // Print array
+    for (int i = 0; i < n; i++)
+    {
+        cout << p[i] << " ";
+    }
+
+    // delete keyword ka use
+    // It del
+    delete ptr;
+    delete ptr2;
+    delete[] p; // it was use for array so [] is used
+}
